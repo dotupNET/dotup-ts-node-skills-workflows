51c1a5b0204ae08e7714b7ee25bbd69a6680aae9
Peter Ullrich <p.ullrich@dotup.de>
2019-03-07 20:33:32 +0100

    Workflow template handler with IWorkflowHandler

--- src/Interfaces/IWorkflowTemplateConfiguration.ts
+++ src/Interfaces/IWorkflowTemplateConfiguration.ts
@@ -1,6 +1,7 @@
 import { WorkflowHandler } from '../WorkflowHandler';
+import { IWorkflowHandler } from './IWorkflowHandler';
 
 export interface IWorkflowTemplateConfiguration<T> {
   WorkflowName: string;
-  Handler: WorkflowHandler<T>;
+  Handler: IWorkflowHandler<T>;
 }
