4c06901b3db78dee41d86052ee5165c0b1640631
Peter Ullrich <dotup.software@gmail.com>
2019-03-30 05:23:32 +0100

    textLibrary

--- src/Plugin/SkillWorkflowsPlugin.ts
+++ src/Plugin/SkillWorkflowsPlugin.ts
@@ -1,9 +1,10 @@
 import { CustomSkillBuilder } from 'ask-sdk-core';
+import { TextLibrary } from 'dotup-ts-internationalization';
 import { LoggerFactory } from 'dotup-ts-logger';
 import { IPlugin } from 'dotup-ts-node-skills';
+import { WorkflowRequestAdapter } from '../WorkflowRequestAdapter';
 import { WorkflowRequestInterceptor } from '../WorkflowRequestInterceptor';
 import { ISkillWorkflowsPluginConfiguration } from './ISkillWorkflowsPluginConfiguration';
-import { WorkflowRequestAdapter } from '../WorkflowRequestAdapter';
 
 export class SkillWorkflowsPlugin<T> implements IPlugin {
   private readonly config: ISkillWorkflowsPluginConfiguration<T>;
@@ -12,7 +13,7 @@ export class SkillWorkflowsPlugin<T> implements IPlugin {
     this.config = config;
   }
 
-  initialize(skillBuilder: CustomSkillBuilder): void {
+  initialize(skillBuilder: CustomSkillBuilder, textLibrary: TextLibrary<string>): void {
     const logger = LoggerFactory.createLogger('SkillWorkflowsPlugin');
     logger.Info('workflow plugin initialized', 'initialize');
 
